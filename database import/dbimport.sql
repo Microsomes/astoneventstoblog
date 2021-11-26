8f22239504282df51996fe313f9d6041c4bc3298
tayyab <tayyab.javed@stickee.co.uk>
2021-11-26 23:29:52 +0000

    all instructions

--- database import/dbimport.sql
+++ database import/dbimport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 20, 2021 at 02:59 AM
+-- Generation Time: Nov 26, 2021 at 10:58 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `newblog2`
+-- Database: `newblog6`
 --
 
 -- --------------------------------------------------------
@@ -33,6 +33,7 @@ CREATE TABLE `wlv_blogs` (
   `title` varchar(255) DEFAULT NULL,
   `author` varchar(255) DEFAULT NULL,
   `content` text,
+  `published` varchar(1) DEFAULT '0',
   `topicId` int NOT NULL,
   `userId` int NOT NULL,
   `createdAT` timestamp NULL DEFAULT CURRENT_TIMESTAMP
@@ -49,6 +50,7 @@ CREATE TABLE `wlv_comments` (
   `comment` text,
   `userId` int NOT NULL,
   `blogId` int NOT NULL,
+  `parent` varchar(16000) DEFAULT NULL,
   `createdAT` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -63,6 +65,21 @@ CREATE TABLE `wlv_likes` (
   `val` int DEFAULT '0',
   `userId` int NOT NULL,
   `blogId` int NOT NULL,
+  `parent` varchar(16000) DEFAULT NULL,
+  `createdAT` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wlv_likes_comments`
+--
+
+CREATE TABLE `wlv_likes_comments` (
+  `id` int NOT NULL,
+  `val` int DEFAULT '0',
+  `userId` int NOT NULL,
+  `commentId` int NOT NULL,
   `createdAT` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -93,13 +110,6 @@ CREATE TABLE `wlv_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `wlv_users`
---
-
-INSERT INTO `wlv_users` (`id`, `username`, `admin`, `password`, `createdAT`) VALUES
-(1, 'microsomes', 0, 'Ta123lo09', '2021-11-20 02:40:14');
-
---
 -- Indexes for dumped tables
 --
 
@@ -126,6 +136,14 @@ ALTER TABLE `wlv_likes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userId` (`userId`),
   ADD KEY `blogId` (`blogId`);
+
+--
+-- Indexes for table `wlv_likes_comments`
+--
+ALTER TABLE `wlv_likes_comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `commentId` (`commentId`);
 
 --
 -- Indexes for table `wlv_topic`
@@ -162,6 +180,12 @@ ALTER TABLE `wlv_likes`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `wlv_likes_comments`
+--
+ALTER TABLE `wlv_likes_comments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `wlv_topic`
 --
 ALTER TABLE `wlv_topic`
@@ -171,7 +195,7 @@ ALTER TABLE `wlv_topic`
 -- AUTO_INCREMENT for table `wlv_users`
 --
 ALTER TABLE `wlv_users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -197,6 +221,13 @@ ALTER TABLE `wlv_comments`
 ALTER TABLE `wlv_likes`
   ADD CONSTRAINT `wlv_likes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `wlv_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `wlv_likes_ibfk_2` FOREIGN KEY (`blogId`) REFERENCES `wlv_blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `wlv_likes_comments`
+--
+ALTER TABLE `wlv_likes_comments`
+  ADD CONSTRAINT `wlv_likes_comments_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `wlv_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `wlv_likes_comments_ibfk_2` FOREIGN KEY (`commentId`) REFERENCES `wlv_comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
